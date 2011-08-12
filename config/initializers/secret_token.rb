3da4cafd6b3fa2b707c188c0406a3f73d7c484e9
Travis Hotz <travishotz@gmail.com>
2011-08-12 02:31:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dockrealty::Application.config.secret_token = 'ee2ad571b1a99166424559caaa399a88d8ff28f661774bd651182a76cfb1e7955896014f486e711041af93f6b1b25e27a3a248b1c5c7527e11827c3de71cee40'
